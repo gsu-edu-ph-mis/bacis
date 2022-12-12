8a4134ce818f1b53ce1a1f263077c124de772405
kosinix <kosinix@users.noreply.github.com>
2022-12-12 14:19:11 +0800

    Use sqlite for session instead of memorystore
    Avoid session resets when restarting app.

---
+++ data/src/db-connect.js
@@ -0,0 +1,25 @@
+//// Core modules
+
+//// External modules
+const { Sequelize } = require('sequelize')
+const moment = require('moment')
+
+module.exports = {
+    connect: async () => {
+        try {
+
+            const sequelize = new Sequelize({
+                dialect: 'sqlite',
+                storage: CONFIG.sqlite.db,
+                logging: false,
+            });
+
+            await sequelize.authenticate()
+            console.log(`${moment().format('YYYY-MMM-DD hh:mm:ss A')}: Database connected.`);
+
+            return sequelize
+        } catch (error) {
+            console.log('Connection error:', error.message)
+        }
+    }
+}
--- data/src/session.js
+++ data/src/session.js
@@ -2,17 +2,19 @@
 
 //// External modules
 const session = require('express-session'); // Session engine
-const MemoryStore = require('memorystore')(session)
+const SessionStore = require('express-session-sequelize')(session.Store);
 
 // Use the session middleware
 // See options in https://github.com/expressjs/session
-module.exports = session({
+module.exports = (database) => {
+  return session({
     name: CONFIG.session.name,
-    store: new MemoryStore({
-      checkPeriod: 86400000 // prune expired entries every 24h
+    store: new SessionStore({
+      db: database,
     }),
     secret: CRED.session.secret,
     cookie: CONFIG.session.cookie,
     resave: false,
     saveUninitialized: false
-});
+  });
+}
